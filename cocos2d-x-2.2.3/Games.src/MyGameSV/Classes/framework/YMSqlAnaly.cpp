62c4fd76a2380ce1a0ba3b632a92a528e2b14fce
qianpeng <512595845@qq.com>
2014-06-08 18:32:17 +0800

    1

--- cocos2d-x-2.2.3/Games.src/MyGameSV/Classes/framework/YMSqlAnaly.cpp
+++
@@ -1,4 +0,0 @@
-#include "YMSqlAnaly.h"
-#include "sqlite3.h"
-#include "YMIconv.h"
-#include "YMSocketData.h"
--- cocos2d-x-2.2.3/Games.src/MyGameSV/Classes/testResource.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TEST_RESOURCE_H_
-#define _TEST_RESOURCE_H_
-
-static const char s_pPathGrossini[]       = "Images/grossini.png";
-
-
-
-#endif
